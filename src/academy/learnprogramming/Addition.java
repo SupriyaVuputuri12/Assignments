77fb1ea69be679acb6037a07ff9006d764a1ba18
SupriyaVuputuri12 <pria.dolly@gmail.com>
2021-09-15 00:07:34 +0530

    Initial commit

---
+++ src/academy/learnprogramming/Addition.java
@@ -0,0 +1,9 @@
+package academy.learnprogramming;
+
+public class Addition {
+    public static void main(String[] args) {
+        int x = 15, y = 20;
+        int sum = x+y;
+        System.out.println("Sum of two numbers X and Y : " + sum);
+    }
+}
---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x = 15, y = 20;
+        System.out.println("Sum of two numbers X and Y : " + x+y);
+    }
+}
